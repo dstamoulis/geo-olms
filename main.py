46b1ef38d621647bffaad6a2f5dba446675cc432
Justin Chung <0921justin@gmail.com>
2025-06-04 17:01:24 -0500

    verified benchmrak 0-10

--- main.py
+++ main.py
@@ -55,6 +55,12 @@ def main():
     query = "Where is 39.3434\u00b0 N, 117.3616\u00b0 E? I want to see it on the map; please zoom there!"
     query = "First, let's load the xView1 images from Signapore for Summer 2017, but please make sure you consider a wide area. Then run the Swin-L detector. Based on the detection counts, which category showed up with more objects in that area: 'Oil Tanker', 'Ferry', or 'Sailboat'? Thanks in advance for the help!"
     query = "Fetch xView1 images from Turkey. How many images are there?"
+    query = "Run the YOLO-v6 LCC model on BigEarthNet from March 2018. Plot the 'Airports' LCC classification results on the map",
+    query = "Plot on the map the xView1 images in (wider area around) Ouagadougou, Burkina Faso, for August 2017",
+    query = "Scatter plot the xView1 images in Kaohsiung, Taiwan from June 2017. I want to make sure that you consider a wide area",
+    query = "Plot on the map the xView1 images in Derby Airport, Australia from August 2017",
+    query = "Run the ResNet-32 LCC model on BigEarthNet from 2nd half of April 2018. How many 'Airports' LCC classification results we got?",
+    query = "Which country has more xView1 images, Greece or Turkey?"
     response = platform.agent.run_query(query)
 
     print(response)

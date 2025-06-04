0616e9061ff78fc4ef65ab1935ed8e8c60831154
Stanley Chung <sc64282@utexas.edu>
2025-06-04 18:11:46 -0500

    removed tool list in orch_agent

--- main_geoolm.py
+++ main_geoolm.py
@@ -92,7 +92,6 @@ def main(args, workflow=None):
         name="orch_agent",
         model_client=model_client,
         messages=messages,
-        toolsets_list=[database, vision, map_tools, data_tools],
         system_message="You are an orchastrating agent handing off tasks to subagents to complete given task!"
     )
     

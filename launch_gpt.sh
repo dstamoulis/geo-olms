8b330166dc643977d61f88183934e3c10c096313
Dimitrios Stamoulis <stamoulis.dimitrios@gmail.com>
2025-06-08 19:01:43 -0500

    tweak

---
+++ launch_gpt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Loop exp_id from 0 to 25 and run the Python script each time
+for i in {0..0}; do
+  python3 main_single.py --exp_id "$i"
+  # python3 main_geoflow.py --exp_id "$i"
+  # python3 main_geoolm.py --exp_id "$i"
+  # python3 main_swarm.py --exp_id "$i"
+done

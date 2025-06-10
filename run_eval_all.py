49f18f5515e72d01345a5f109c4f6af524389fb1
Dimitrios Stamoulis <stamoulis.dimitrios@gmail.com>
2025-06-10 17:24:54 -0500

    regex fix

--- run_eval_all.py
+++ run_eval_all.py
@@ -1,23 +1,22 @@
 from evaluate.agent_metrics import AgentMetrics
 import os
+from utils import re_args_component
 
 def main():
 
     gts_path = "results/flow_gt/openai/gpt_4o_mini/single_agent/"
 
     run_client = "openai" # openai, ollama
-    run_model = "gpt_4o_mini"
+    run_model = "gpt-4o-mini"
+    run_flow_ver = run_model #"flow_gt"
 
-    run_flow_ver = "flow_gt"
     run_methods = ['single_agent', 'geoflow', 'geoolm', 'swarm', 'seq_stateflow', 'group_stateflow']
     run_methods = ['geoflow', 'geoolm', 'swarm', 'seq_stateflow', 'group_stateflow']
-
-    # run_flow_ver = "gpt_4o_mini"
-    # run_methods = ['geoflow']
+    run_methods = ['geoflow']
 
     for run_method in run_methods:
 
-        results_path = f"results/{run_flow_ver}/{run_client}/{run_model}/{run_method}/"
+        results_path = f"results/{re_args_component(run_flow_ver)}/{re_args_component(run_client)}/{re_args_component(run_model)}/{re_args_component(run_method)}/"
         agent_metrics = AgentMetrics(gts_path, results_path)
 
         runs_from, runs_to = 0, 22
